4a1a94c5a36fdbf8c3b357151a8759be836e6782
Lena0912 <elenabelichenko9@gmail.com>
2024-08-25 00:06:39 +0300

    react-app

---
+++ src/videos.js
@@ -0,0 +1,52 @@
+import reactLogo from "./assets/react.svg";
+
+export const VIDEOS = [
+  {
+    id: 1,
+    title: "Video 1",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+  {
+    id: 2,
+    title: "Video 2",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+  {
+    id: 3,
+    title: "Video 3",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+  {
+    id: 4,
+    title: "Video 4",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+  {
+    id: 5,
+    title: "Video 5",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+  {
+    id: 6,
+    title: "Video 6",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+  {
+    id: 7,
+    title: "Video 7",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+  {
+    id: 8,
+    title: "Video 8",
+    channelName: "Belichenko JS",
+    img: reactLogo,
+  },
+];
